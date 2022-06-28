1a89f88f6e6fdfbed2f8ab41f40855e93fb46251
saeel.naik <saeel.naik@teknorix.com>
2022-06-28 17:40:08 +0530

    changes

---
+++ OrganizationManagementTool/wwwroot/js/AngularController/HomeAngularJS.js
@@ -0,0 +1,20 @@
+﻿
+var app = angular.module("FacultyApp", []);
+
+app.controller("FacultyController", function ($scope, $http){
+    $scope.btnsave = "Save";
+    $.savedata = function () {
+        $scope.btnsave = "Please wait...";
+        $http({
+            method: 'POST',
+            url: 'Faculty/AddFaculty',
+            data: $scope.FacultyModel
+        }).success(function (result) {
+            $scope.btnsave = "Save";
+            $scope.FacultyModel = null;
+            alert(result);
+        }).error(function () {
+            alert('Failed');
+        })
+    }
+});
